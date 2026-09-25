0cb3d7c9731762d496c1cdb1fbd7e6a612d6e89d
phuchoangvo210405 <phuchoangvo210405@gmail.com>
2026-09-25 14:38:58 +0700

    TASK-350-Viet-API-Quan-ly-Goi-tap-Lap-Hop-đong-moi

---
+++ QLDACNTTnhom10/DTOs/CreateContractRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace QLDACNTTnhom10.DTOs
+{
+    public class CreateContractRequest
+    {
+        public int MemberID { get; set; }
+        public int PackageID { get; set; }
+        public int? TrainerID { get; set; } // NULL nếu tập Gym tự do
+        public int? ClassID { get; set; }   // NULL nếu không tập Yoga
+        public decimal? DiscountAmount { get; set; }
+        public string? PromoCode { get; set; }
+        public string PaymentMethod { get; set; } // Tiền mặt, VNPAY, MoMo
+    }
+}
